cc9787bd459886b17f1669ea292baa1209214a33
RJ_Infinity <rjinfinity2004@gmail.com>
2023-07-03 16:36:13 +0100

    strip back all css and rewrite index css (+ other improvments)

--- script.js
+++ script.js
@@ -33,27 +33,45 @@ menuCheckbox.addEventListener('change', e => {
 });
 menuLabel = document.querySelector(".menu[for=\"menuCheckbox\"]")
 menuLabel.addEventListener("keydown", e => {
-	console.log("asdf")
-	if (e.key == " " || e.key == "Enter") {
+	if (e.key == " " || e.key == "Enter" || (menuCheckbox.checked && e.key == "Escape")) {
 		menuCheckbox.checked = !menuCheckbox.checked;
 		menuCheckbox.dispatchEvent(new Event("change"));
+		e.stopPropagation();
+		e.stopImmediatePropagation();
+		e.preventDefault();
 	}
 	if (e.key == "ArrowDown" && menuCheckbox.checked) {
 		document.querySelector("#menuContent > a:first-child").focus()
+		e.stopPropagation();
+		e.stopImmediatePropagation();
+		e.preventDefault();
 	}
 });
 
 [...menuContent.children].forEach(el => {
 	el.addEventListener("keydown", e => {
 		if (e.key == "ArrowDown" && menuCheckbox.checked && e.target.nextElementSibling != null) {
-			e.target.nextElementSibling.focus()
+			e.target.nextElementSibling.focus();
+			e.stopPropagation();
+			e.stopImmediatePropagation();
+			e.preventDefault();
 		}
 		if (e.key == "ArrowUp" && menuCheckbox.checked) {
 			if (e.target.previousElementSibling == null) {
-				menuLabel.focus()
+				menuLabel.focus();
 			} else {
 				e.target.previousElementSibling.focus()
 			}
+			e.stopPropagation();
+			e.stopImmediatePropagation();
+			e.preventDefault();
+		}
+		if (e.key == "Escape") {
+			menuCheckbox.checked = !menuCheckbox.checked;
+			menuCheckbox.dispatchEvent(new Event("change"));
+			e.stopPropagation();
+			e.stopImmediatePropagation();
+			e.preventDefault();
 		}
 	})
 });
